4d9436df7fa41bc4bc32d540523b37a27232c863
Cornel <filipcornelcristian@gmail.com>
2020-05-06 17:31:36 +0300

    implement transition events (introstart, introend, outrostart, outroend)

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   import Spring from "./Spring.svelte";
 
   let boxInput;
+  let showParagraph = false;
 
   const progress = tweened(0, {
     delay: 0,
@@ -42,19 +43,40 @@
 
 <!-- <progress value={$progress} /> -->
 <!-- <Spring /> -->
+
+<button
+  on:click={() => {
+    showParagraph = !showParagraph;
+  }}>
+  Toggle
+</button>
+{#if showParagraph}
+  <p transition:fly={{ x: 300 }}>Can you see me?</p>
+{/if}
+
+<hr />
+
 <input type="text" bind:this={boxInput} />
 <button on:click={addBox}>Add</button>
 
-{#each boxes as box (box)}
-  <!-- <div transition:fade>{box}</div> -->
-  <!-- <div transition:scale>{box}</div> -->
-  <!-- <div transition:slide>{box}</div> -->
-  <!-- <div transition:fly={{ easing: cubicIn }}>{box}</div> -->
-  <!-- <div transition:scale={{ easing: cubicIn, start: 0.5, opacity: 0.5 }}>
+{#if showParagraph}
+  {#each boxes as box (box)}
+    <!-- <div transition:fade>{box}</div> -->
+    <!-- <div transition:scale>{box}</div> -->
+    <!-- <div transition:slide>{box}</div> -->
+    <!-- <div transition:fly={{ easing: cubicIn }}>{box}</div> -->
+    <!-- <div transition:scale={{ easing: cubicIn, start: 0.5, opacity: 0.5 }}>
     {box}
   </div> -->
-  <!-- <div transition:fly={{ easing: cubicIn, x: 0, y: 300 }}>{box}</div> -->
-  <div transition:fly={{ x: 200, y: 0 }} on:click={discard.bind(this, box)}>
-    {box}
-  </div>
-{/each}
+    <!-- <div transition:fly={{ easing: cubicIn, x: 0, y: 300 }}>{box}</div> -->
+    <div
+      transition:fly|local={{ x: 200, y: 0 }}
+      on:click={discard.bind(this, box)}
+      on:introstart={() => console.log('adding the element starts')}
+      on:introend={() => console.log('adding the element end')}
+      on:outrostart={() => console.log('remove the element starts')}
+      on:outroend={() => console.log('remove the element end')}>
+      {box}
+    </div>
+  {/each}
+{/if}
